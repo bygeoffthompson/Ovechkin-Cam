01f121e3747f12368d77e068a1740333f2027f23
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-05-06 15:52:27 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="goals">
+        <form class="goal-search">
+          <label htmlFor="goal">Goal Number:</label>
+          <input type="number" id="goal" name="goal" min="1" max="897"/>
+          <input type="submit" value="Submit"/>
+        </form>
+        <div className="goal" id="goal-1">
+          <p>Goal 1</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/BoGOoO9WTOE?si=OdZBpn3y3W7un6r0" title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen></iframe>
+        </div>
+        <div className="goal" id="goal-1">
+          <p>Goal 100</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/F7jFSVOVWyU?si=CnWW9ylYbomIzBGM" title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen></iframe>
+        </div>
+      </div>
   );
 }
 
