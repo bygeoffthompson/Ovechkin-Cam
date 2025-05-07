1eb37472c681f6e117baefb5cf6a7357da42974f
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-05-07 16:44:50 -0400

    .

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function SearchForm({jsonData}) {
             {searchResults.map((result, index) => (
                 <div key={index}>
                     <p>Watch goal {JSON.stringify(result.goal).replace(/"/g, "")}.</p>
-                    <iframe width="560" height="315" src={JSON.stringify(result.link).replace(/"/g, "")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+                    <iframe width="560" height="315" src={JSON.stringify(result.link).replace(/"/g, "")} title="YouTube video player" referrerPolicy="strict-origin-when-cross-origin"></iframe>
                 </div>
             ))}
         </div>
