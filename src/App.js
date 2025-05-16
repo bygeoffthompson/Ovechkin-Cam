8cfcaaf9578ccab5c85e11442b915d16f56e6c7a
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-05-16 18:34:30 -0400

    .

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function SearchForm({jsonData}) {
     };
 
     const highlightGoal = () => {
-        const highlights = ['bookmark', 'bullseye', 'four', 'fifty', 'hat', 'mega', 'penalty', 'sixty', 'trophy'];
+        const highlights = ['bookmark', 'bullseye', 'four', 'fifty', 'forty', 'hat', 'mega', 'penalty', 'sixty', 'trophy'];
 
         const result = jsonData.filter(item =>
             Object.values(item).some(value =>
