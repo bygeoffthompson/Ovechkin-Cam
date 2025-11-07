d4507bd489cbb9e75d4b2e78a3ce66adb0763f86
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-11-07 13:18:12 -0500

    Tabindex

--- src/App.js
+++ src/App.js
@@ -182,13 +182,13 @@ function SearchForm({jsonData}) {
                 <div>
                     <h2>Search</h2>
                     <label htmlFor="search-goal">Number</label>
-                    <input id="search-goal" title="Regular Season" type="text" placeholder="#" value={searchGoal} onChange={handleGoalChange}/>
+                    <input id="search-goal" tabIndex="0" type="number" placeholder="#" value={searchGoal} onChange={handleGoalChange}/>
                     <label htmlFor="search-text-1">Text</label>
                     <label htmlFor="search-text-2" hidden>Text</label>
                     <label htmlFor="search-text-3" hidden>Text</label>
-                    <input id="search-text-1" title="All" type="text" placeholder="Search" value={searchText1} onChange={handleText1}/>
-                    <input id="search-text-2" type="text" placeholder="And" value={searchText2} onChange={handleText2}/>
-                    <input id="search-text-3" type="text" placeholder="And" value={searchText3} onChange={handleText3}/>
+                    <input id="search-text-1" tabIndex="1" type="text" placeholder="Search" value={searchText1} onChange={handleText1}/>
+                    <input id="search-text-2" tabIndex="2" type="text" placeholder="And" value={searchText2} onChange={handleText2}/>
+                    <input id="search-text-3" tabIndex="3" type="text" placeholder="And" value={searchText3} onChange={handleText3}/>
                 </div>
                 <div>
                     <strong>or Randomize</strong>
