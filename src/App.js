91b71ded43925a1abe46800984ffa57c1b7eeb96
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-11-20 21:49:50 -0500

    907

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga4';
 
 ReactGA.initialize('G-5RVBYX6N0S');
 
-const totalGoals = 905;
+const totalGoals = 907;
 
 function SearchForm({jsonData}) {
     const [searchGoal, setSearchGoal] = useState('');
