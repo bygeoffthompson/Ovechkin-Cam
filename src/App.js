0b511353443b7dfe3c26871003ae7078b609f05e
Geoff Thompson <gthompson@unleashed-technologies.com>
2025-12-06 22:42:07 -0500

    Revert "Old reset"
    This reverts commit ea91d00793dc4b26fd01be6534b89a6482a5e747.

--- src/App.js
+++ src/App.js
@@ -187,6 +187,16 @@ function SearchForm({jsonData}) {
         setSearchGoal(goal[0]);
     }
 
+    const reset = () => {
+        document.getElementById('minimum').classList.remove('show');
+        document.getElementById('column').value = '1';
+        document.getElementById('wrapper').classList.remove('column-1', 'column-2', 'column-3');
+        document.getElementById('season').value = '';
+        setSearchGoal('');
+        setSearchResults([]);
+        resultsHide();
+    };
+
     function resultsHide() {
         setSearchText1('');
         setSearchText2('');
@@ -263,7 +273,7 @@ function SearchForm({jsonData}) {
                         <button onClick={(event) => setSearchGoal(random(854, totalGoals))} className="coach-button" title="Spencer Carbery" type="button">Carbery</button>
                     </div>
                 </div>
-                <a href="/" title="Reset">Reset</a>
+                <button onClick={reset} title="Reset" type="button">Reset</button>
             </form>
 
             <div className="search-accordion" id="minimum">
