adffab18f8f7b0bddd4f5ed869680d3d00ea1186
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-01-09 22:27:01 -0500

    916

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ if (window.location.hostname !== 'localhost') {
     ReactGA.initialize('G-5RVBYX6N0S');
 }
 
-const totalGoals = 915;
+const totalGoals = 916;
 
 function SearchForm({jsonData}) {
     const [searchGoal, setSearchGoal] = useState('');
